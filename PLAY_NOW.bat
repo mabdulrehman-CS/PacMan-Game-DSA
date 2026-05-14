d7d361e6c8a6a5d9b9a9210b039d384f4c8ce991
mabdulrehman-CS <abdulrehmanmuhammad524@gmail.com>
2026-05-14 12:19:44 +0500

    Add PLAY_NOW.bat

---
+++ PLAY_NOW.bat
@@ -0,0 +1,20 @@
+@echo off
+cls
+echo ====================================
+echo   AlgoMan - Console Edition
+echo   STARTING NOW!
+echo ====================================
+echo.
+echo Controls:
+echo   W/A/S/D or Arrow Keys = Move
+echo   U = Undo last move
+echo   Q = Quit
+echo.
+echo All DSA structures working:
+echo   - Graph (Maze)
+echo   - BFS/Dijkstra (Ghost AI)
+echo   - Stack (Undo)
+echo   - LinkedList (Pellets)
+echo.
+pause
+AlgoMan_Console.exe
